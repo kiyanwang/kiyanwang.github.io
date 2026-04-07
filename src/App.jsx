d2c62f1a2833e363cf065757bc7781f53e4403d9
Nadeem Shabir <nadeem.shabir+kiyanwang@gmail.com>
2026-04-07 10:02:47 +0100

    feat: wire up routing for all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
+import Projects from './pages/Projects';
+import Writing from './pages/Writing';
+
 export default function App() {
-  return <div>Portfolio</div>;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/writing" element={<Writing />} />
+      </Routes>
+    </HashRouter>
+  );
 }
